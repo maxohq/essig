15c511ea6b298bc6f3901cc99278018773670816
Roman Heinrich <roman.heinrich@gmail.com>
2024-08-15 01:55:24 +0200

    Chore: some cleanup

--- lib/child_registry.ex
+++ lib/child_registry.ex
@@ -1,4 +1,6 @@
 defmodule ChildRegistry do
+  require Logger
+
   def register({app_name, module}, pid) do
     Registry.register(__MODULE__, {app_name, module}, pid)
   end
@@ -9,6 +11,20 @@ defmodule ChildRegistry do
     case Registry.lookup(__MODULE__, {app_name, module}) do
       [{pid, _}] -> pid
       [] -> nil
+    end
+  end
+
+  def ensure_started do
+    case Registry.start_link(keys: :unique, name: ChildRegistry) do
+      {:ok, _} ->
+        :ok
+
+      {:error, {:already_started, _}} ->
+        :ok
+
+      error ->
+        Logger.error("Failed to start ChildRegistry: #{inspect(error)}")
+        throw({:error, :registry_start_failed})
     end
   end
 end
--- lib/starter.ex
+++ lib/starter.ex
@@ -6,37 +6,43 @@ defmodule Starter do
     supervisor_name = String.to_atom("#{app_name}_Supervisor")
 
     # Ensure ChildRegistry is started
-    case Registry.start_link(keys: :unique, name: ChildRegistry) do
-      {:ok, _} -> :ok
-      {:error, {:already_started, _}} -> :ok
-      error -> 
-        Logger.error("Failed to start ChildRegistry: #{inspect(error)}")
-        throw({:error, :registry_start_failed})
-    end
+    ChildRegistry.ensure_started()
 
-    children =
-      Enum.map(modules, fn module ->
-        %{
-          id: {app_name, module},
-          start: {module, :start_link, [[]]}
-        }
-      end)
-
+    children = children_specs(app_name, modules)
     Logger.info("Children to be supervised: #{inspect(children)}")
 
+    if app_name != nil do
+      start_children(children, supervisor_name)
+    else
+      Logger.error("Set the current app via Context.set_current_app()!")
+    end
+  end
+
+  def start_children(children, supervisor_name) do
     opts = [strategy: :one_for_one, name: supervisor_name]
-    
+
     case Supervisor.start_link(children, opts) do
       {:ok, pid} ->
         Logger.info("Supervisor started successfully with PID: #{inspect(pid)}")
         {:ok, pid}
+
       {:error, {:shutdown, reason}} ->
         Logger.error("Failed to start supervisor due to child failure: #{inspect(reason)}")
         {:error, {:child_failure, reason}}
+
       {:error, reason} ->
         Logger.error("Failed to start supervisor: #{inspect(reason)}")
         {:error, reason}
     end
+  end
+
+  def children_specs(app_name, modules) do
+    Enum.map(modules, fn module ->
+      %{
+        id: {app_name, module},
+        start: {module, :start_link, [[]]}
+      }
+    end)
   end
 
   def stop_supervisor do
