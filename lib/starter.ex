d64d757369e98961cef24cb7169297f20b091b38
Roman Heinrich <roman.heinrich@gmail.com>
2024-08-15 02:01:58 +0200

    Chore: now it is possible to add new children

--- lib/starter.ex
+++ lib/starter.ex
@@ -1,6 +1,15 @@
 defmodule Starter do
   require Logger
 
+  def stop_supervisor do
+    app_name = Context.current_app()
+    supervisor_name = String.to_atom("#{app_name}_Supervisor")
+
+    if Process.whereis(supervisor_name) do
+      Supervisor.stop(supervisor_name)
+    end
+  end
+
   def supervise_modules(modules) do
     app_name = Context.current_app()
     supervisor_name = String.to_atom("#{app_name}_Supervisor")
@@ -18,7 +27,20 @@ defmodule Starter do
     end
   end
 
-  def start_children(children, supervisor_name) do
+  def add_modules(modules) do
+    app_name = Context.current_app()
+    supervisor_name = String.to_atom("#{app_name}_Supervisor")
+
+    if Process.whereis(supervisor_name) do
+      new_children = children_specs(app_name, modules)
+      add_children(supervisor_name, new_children)
+    else
+      Logger.error("Supervisor not found. Start it first with supervise_modules/1")
+      {:error, :supervisor_not_found}
+    end
+  end
+
+  defp start_children(children, supervisor_name) do
     opts = [strategy: :one_for_one, name: supervisor_name]
 
     case Supervisor.start_link(children, opts) do
@@ -36,21 +58,28 @@ defmodule Starter do
     end
   end
 
-  def children_specs(app_name, modules) do
+  defp add_children(supervisor_name, new_children) do
+    Enum.reduce(new_children, {:ok, []}, fn child_spec, {status, started} ->
+      case Supervisor.start_child(supervisor_name, child_spec) do
+        {:ok, _pid} ->
+          Logger.info("Started new child: #{inspect(child_spec.id)}")
+          {status, [child_spec.id | started]}
+        {:error, {:already_started, _pid}} ->
+          Logger.info("Child already running: #{inspect(child_spec.id)}")
+          {status, started}
+        {:error, reason} ->
+          Logger.error("Failed to start child #{inspect(child_spec.id)}: #{inspect(reason)}")
+          {:error, started}
+      end
+    end)
+  end
+
+  defp children_specs(app_name, modules) do
     Enum.map(modules, fn module ->
       %{
         id: {app_name, module},
         start: {module, :start_link, [[]]}
       }
     end)
-  end
-
-  def stop_supervisor do
-    app_name = Context.current_app()
-    supervisor_name = String.to_atom("#{app_name}_Supervisor")
-
-    if Process.whereis(supervisor_name) do
-      Supervisor.stop(supervisor_name)
-    end
   end
 end
