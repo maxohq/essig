c884cf3915b8500533a320c224fb2aac713e1812
Roman Heinrich <roman.heinrich@gmail.com>
2024-08-16 05:55:10 +0200

    chore: remove cruft

--- lib/test_helper.exs
+++ lib/test_helper.exs
@@ -1,12 +1,1 @@
 ExUnit.start(trace: false)
-
-ExUnit.configure(exclude: [:skip])
-
-# Setup for all tests
-ExUnit.after_suite(fn _ ->
-  # Reset the Context after all tests
-  Context.set_current_app(nil)
-end)
-
-# Ensure Supernamed application is started for tests
-Application.ensure_all_started(:supernamed)
