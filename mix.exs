232422d6cf0d5fc0ad528ee4b850978be5a029d2
Roman Heinrich <roman.heinrich@gmail.com>
2024-08-22 18:07:44 +0200

    Chore: elixir 1.16

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Essig.MixProject do
     [
       app: :essig,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
