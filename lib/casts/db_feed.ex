a1e90ad7c95c197c7eaa0099e3b76268cdd8a374
Roman Heinrich <roman.heinrich@gmail.com>
2024-08-25 00:18:49 +0200

    Chore: refactoring

--- lib/casts/db_feed.ex
+++ lib/casts/db_feed.ex
@@ -2,13 +2,7 @@ defmodule Essig.Casts.DbFeed do
   use Essig.Repo
 
   def hydrate_cast(module) do
-    scope_uuid = Essig.Context.current_scope()
-
-    query =
-      from e in Essig.Schemas.Event, where: e.scope_uuid == ^scope_uuid, order_by: [asc: :id]
-
-    stream = query |> Repo.cursor_based_stream(max_rows: 100)
-
+    stream = scoped_events() |> Repo.cursor_based_stream(max_rows: 100)
     Essig.Casts.CastRunner.send_events(module, stream)
 
     Enum.reduce(stream, %{}, fn _event, _acc ->
@@ -16,5 +10,10 @@ defmodule Essig.Casts.DbFeed do
       # entity = Vecufy.TestReports.Handler.handle(acc, event)
       # %TestReportProcess{entity | seq: event.seq}
     end)
+  end
+
+  def scoped_events do
+    scope_uuid = Essig.Context.current_scope()
+    from e in Essig.Schemas.Event, where: e.scope_uuid == ^scope_uuid, order_by: [asc: :id]
   end
 end
