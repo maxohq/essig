0ace7e672f10db652e9f0b97d00fa359fdae7490
Roman Heinrich <roman.heinrich@gmail.com>
2024-09-05 02:52:00 +0200

    Chore: Checker allows appending to same stream

--- lib/checker.ex
+++ lib/checker.ex
@@ -4,14 +4,18 @@ defmodule Essig.Checker do
   """
   def run do
     scope_uuid = Essig.UUID7.generate()
+    stream_uuid = Essig.UUID7.generate()
+    run(scope_uuid, stream_uuid)
+  end
 
+  def run(scope_uuid, stream_uuid) do
     Essig.Server.start_scope(scope_uuid)
     Essig.Server.start_casts([SampleCast1])
 
-    stream_uuid = Essig.UUID7.generate()
+    seq = Essig.EventStore.last_seq(stream_uuid)
 
     {:ok, %{events: events}} =
-      Essig.EventStore.append_to_stream(stream_uuid, "trp", 0, [
+      Essig.EventStore.append_to_stream(stream_uuid, "trp", seq, [
         %Sample.TestReports.Events.MasterReportAdded{path: "local/path/to", report: "report 1"},
         %Sample.TestReports.Events.MasterReportAdded{path: "local/path/to", report: "report 2"},
         %Sample.TestReports.Events.MasterReportAdded{path: "local/path/to", report: "report 3"}
