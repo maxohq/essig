489d09f9ba73b4e0131868776f9a0bd59956e5e4
Roman Heinrich <roman.heinrich@gmail.com>
2024-09-07 00:29:49 +0200

    Chore: usage examples

--- lib/event_store_reads.ex
+++ lib/event_store_reads.ex
@@ -2,6 +2,12 @@ defmodule Essig.EventStoreReads do
   @moduledoc """
   EventStoreReads sets current scope explicitly, for reads.
   This makes it possible to cache with [GenCache](https://github.com/maxohq/gen_cache/)
+
+  Usage:
+  ```
+   iex> Essig.Cache.request({Essig.EventStoreReads, :read_all_stream_forward, ["0191be93-c178-71a0-8bd8-bcde9f55b7d6", 5,10]})
+   iex> Essig.Cache.request({Essig.EventStoreReads, :read_stream_forward, ["0191be93-c178-71a0-8bd8-bcde9f55b7d6", "0191be93-c178-7203-b137-c4e294ca925b", 5, 10]})
+  ```
   """
   use Essig.Repo
 
