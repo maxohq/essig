e0ac820e2222296593b440441bc7def7b8e99c56
Roman Heinrich <roman.heinrich@gmail.com>
2024-09-13 00:14:32 +0200

    Chore: tests for projection db update

--- lib/crud/projections_crud_test.exs
+++ lib/crud/projections_crud_test.exs
@@ -12,16 +12,16 @@ defmodule Essig.ProjectionsCrudTest do
   test "provides defaults for numeric values" do
     uuid = Essig.UUID7.generate()
 
-    {:ok, cast} =
+    {:ok, projection} =
       ProjectionsCrud.create_projection(%{module: "module-1", scope_uuid: uuid, seq: 1})
 
-    assert cast.status == :new
+    assert projection.status == :new
   end
 
-  test "prevents duplicate casts with same module" do
+  test "prevents duplicate projections with same module" do
     uuid = Essig.UUID7.generate()
 
-    {:ok, _cast} =
+    {:ok, _projection} =
       ProjectionsCrud.create_projection(%{module: "module-1", scope_uuid: uuid, seq: 1})
 
     {:error, changeset} =
@@ -31,13 +31,42 @@ defmodule Essig.ProjectionsCrudTest do
     assert errors == %{scope_uuid: ["has already been taken"]}
   end
 
+  test "can be fetched by module name" do
+    scope_uuid = Essig.UUID7.generate()
+    Essig.Context.set_current_scope(scope_uuid)
+
+    {:ok, projection} =
+      ProjectionsCrud.create_projection(%{module: "Module1", scope_uuid: scope_uuid, seq: 1})
+
+    copy = ProjectionsCrud.get_projection_by_module("Module1")
+
+    assert copy == projection
+  end
+
+  test "updates work ok" do
+    scope_uuid = Essig.UUID7.generate()
+    Essig.Context.set_current_scope(scope_uuid)
+
+    {:ok, p1} =
+      ProjectionsCrud.create_projection(%{module: "Module1", scope_uuid: scope_uuid, seq: 1})
+
+    {:ok, p2} = ProjectionsCrud.update_projection(p1, %{seq: 2, max_id: 5})
+    assert p2.seq == 2
+    assert p2.max_id == 5
+    {:ok, p3} = ProjectionsCrud.update_projection(p2, %{seq: 3})
+    assert p3.seq == 3
+    {:ok, p4} = ProjectionsCrud.update_projection(p3, %{seq: 4, status: "idle"})
+    assert p4.seq == 4
+    assert p4.status == :idle
+  end
+
   test "allows updating the `max_id` value via upsert on (module)" do
     uuid = Essig.UUID7.generate()
 
-    {:ok, cast} =
+    {:ok, projection} =
       ProjectionsCrud.create_projection(%{module: "module-1", scope_uuid: uuid, seq: 1})
 
-    {:ok, cast2} =
+    {:ok, projection2} =
       ProjectionsCrud.upsert_projection(%{
         module: "module-1",
         scope_uuid: uuid,
@@ -46,9 +75,9 @@ defmodule Essig.ProjectionsCrudTest do
         status: :backfilling
       })
 
-    assert cast2.max_id == 2
+    assert projection2.max_id == 2
 
-    {:ok, cast3} =
+    {:ok, projection3} =
       ProjectionsCrud.upsert_projection(%{
         module: "module-1",
         scope_uuid: uuid,
@@ -57,10 +86,10 @@ defmodule Essig.ProjectionsCrudTest do
         status: :backfilling
       })
 
-    assert cast3.seq == 5
-    assert cast3.max_id == 7
+    assert projection3.seq == 5
+    assert projection3.max_id == 7
     # same DB record!
-    assert cast.id == cast2.id
-    assert cast.id == cast3.id
+    assert projection.id == projection2.id
+    assert projection.id == projection3.id
   end
 end
