ad0c0b4455611f231f1bc2d51c0faed4e341ab0c
Roman Heinrich <roman.heinrich@gmail.com>
2024-09-12 23:49:37 +0200

    Chore: simple example

--- lib/sample/projections/proj1.ex
+++ lib/sample/projections/proj1.ex
@@ -6,7 +6,7 @@ defmodule Sample.Projections.Proj1 do
   @impl Projections.Projection
   def handle_event(multi, {map, index}) do
     Ecto.Multi.run(multi, {:event, index}, fn _repo, _changes ->
-      IO.inspect(map, label: "index-#{index}")
+      IO.inspect(map.data, label: "index-#{index}")
       {:ok, 1}
     end)
   end
@@ -14,6 +14,7 @@ defmodule Sample.Projections.Proj1 do
   @impl Projections.Projection
   def init_storage(data = %Data{}) do
     Logger.info("RUNNING INIT STORAGE for #{__MODULE__} with name #{inspect(data.name)}")
+    Repo.query("create table if not exists projection_proj1 (id integer, data text)")
     :ok
   end
 end
