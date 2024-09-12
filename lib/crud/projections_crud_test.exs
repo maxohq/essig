bfde363630b083135a48385e26a6548e9fc48155
Roman Heinrich <roman.heinrich@gmail.com>
2024-09-12 23:49:21 +0200

    Feat: rename casts to projections

--- lib/crud/projections_crud_test.exs
+++ lib/crud/projections_crud_test.exs
@@ -1,9 +1,9 @@
-defmodule Essig.CastsCrudTest do
+defmodule Essig.ProjectionsCrudTest do
   use Essig.DataCase
-  alias Essig.Crud.CastsCrud
+  alias Essig.Crud.ProjectionsCrud
 
   test "requires name field" do
-    {:error, changeset} = CastsCrud.create_cast(%{})
+    {:error, changeset} = ProjectionsCrud.create_projection(%{})
     errors = errors_on(changeset)
     assert Map.keys(errors) == [:module, :seq, :scope_uuid]
     assert Map.get(errors, :module) == ["can't be blank"]
@@ -11,24 +11,34 @@ defmodule Essig.CastsCrudTest do
 
   test "provides defaults for numeric values" do
     uuid = Essig.UUID7.generate()
-    {:ok, cast} = CastsCrud.create_cast(%{module: "module-1", scope_uuid: uuid, seq: 1})
+
+    {:ok, cast} =
+      ProjectionsCrud.create_projection(%{module: "module-1", scope_uuid: uuid, seq: 1})
+
     assert cast.status == :new
   end
 
   test "prevents duplicate casts with same module" do
     uuid = Essig.UUID7.generate()
-    {:ok, _cast} = CastsCrud.create_cast(%{module: "module-1", scope_uuid: uuid, seq: 1})
-    {:error, changeset} = CastsCrud.create_cast(%{module: "module-1", scope_uuid: uuid, seq: 2})
+
+    {:ok, _cast} =
+      ProjectionsCrud.create_projection(%{module: "module-1", scope_uuid: uuid, seq: 1})
+
+    {:error, changeset} =
+      ProjectionsCrud.create_projection(%{module: "module-1", scope_uuid: uuid, seq: 2})
+
     errors = errors_on(changeset)
     assert errors == %{scope_uuid: ["has already been taken"]}
   end
 
   test "allows updating the `max_id` value via upsert on (module)" do
     uuid = Essig.UUID7.generate()
-    {:ok, cast} = CastsCrud.create_cast(%{module: "module-1", scope_uuid: uuid, seq: 1})
+
+    {:ok, cast} =
+      ProjectionsCrud.create_projection(%{module: "module-1", scope_uuid: uuid, seq: 1})
 
     {:ok, cast2} =
-      CastsCrud.upsert_cast(%{
+      ProjectionsCrud.upsert_projection(%{
         module: "module-1",
         scope_uuid: uuid,
         max_id: 2,
@@ -39,7 +49,7 @@ defmodule Essig.CastsCrudTest do
     assert cast2.max_id == 2
 
     {:ok, cast3} =
-      CastsCrud.upsert_cast(%{
+      ProjectionsCrud.upsert_projection(%{
         module: "module-1",
         scope_uuid: uuid,
         seq: 5,
--- lib/migrations/all.ex
+++ lib/migrations/all.ex
@@ -7,7 +7,8 @@ defmodule Migrations.All do
       {2024_0824_120000, Migrations.Migration001},
       {2024_0904_112600, Migrations.Migration002},
       {2024_0904_114100, Migrations.Migration003},
-      {2024_0907_232900, Migrations.Migration004}
+      {2024_0907_232900, Migrations.Migration004},
+      {2024_0912_234500, Migrations.Migration005}
     ]
   end
 end
