e382396ce870d9e479a2b07f91797b8cb2a5e823
Roman Heinrich <roman.heinrich@gmail.com>
2024-12-25 20:59:45 +0100

    Chore: Sample.StartSample module to quickly tests projections

---
+++ lib/sample/start_sample.ex
@@ -0,0 +1,21 @@
+defmodule Sample.StartSample do
+  def start do
+    scope_uuid = Essig.UUID7.generate()
+    Essig.Context.set_current_scope(scope_uuid)
+    Essig.Server.start_scope(scope_uuid)
+    ## start projections
+    Essig.Server.start_projections([Sample.Projections.Proj1], pause_ms: 2)
+  end
+
+  def add_events do
+    ## insert events
+    events = [
+      Sample.TestReports.Events.Event1.new(payload: "hello"),
+      Sample.TestReports.Events.Event2.new(ops: [%{op: "a"}])
+    ]
+
+    stream_uuid = Essig.UUID7.generate()
+
+    Essig.EventStore.append_to_stream(stream_uuid, "trp", 0, events)
+  end
+end
