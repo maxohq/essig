25a1c6208b9cd6e1803677264bf6ddf80f376c6f
Roman Heinrich <roman.heinrich@gmail.com>
2024-12-25 20:59:10 +0100

    Chore: make sample events more generic

--- lib/checker.ex
+++ lib/checker.ex
@@ -15,9 +15,9 @@ defmodule Essig.Checker do
 
     {:ok, %{events: _events}} =
       Essig.EventStore.append_to_stream(stream_uuid, "trp", seq, [
-        %Sample.TestReports.Events.MasterReportAdded{path: "local/path/to", report: "report 1"},
-        %Sample.TestReports.Events.MasterReportAdded{path: "local/path/to", report: "report 2"},
-        %Sample.TestReports.Events.MasterReportAdded{path: "local/path/to", report: "report 3"}
+        %Sample.TestReports.Events.Event3{path: "local/path/to"},
+        %Sample.TestReports.Events.Event3{path: "local/path/to"},
+        %Sample.TestReports.Events.Event3{path: "local/path/to"}
       ])
   end
 end
