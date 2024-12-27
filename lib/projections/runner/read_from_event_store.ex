ff5760063b504cd061e7ee0fd20312a7c3e31950
Roman Heinrich <roman.heinrich@gmail.com>
2024-12-27 20:26:33 +0100

    Feat: adjust module namespace + reduce log level to debug for Projections.Runner

--- lib/projections/runner/read_from_event_store.ex
+++ lib/projections/runner/read_from_event_store.ex
@@ -1,10 +1,9 @@
-defmodule Projections.Runner.ReadFromEventStore do
+defmodule Essig.Projections.Runner.ReadFromEventStore do
   alias Essig.Projections.Data
-  alias Projections.Runner.Common
+  alias Essig.Projections.Runner.Common
   require Logger
 
   def run(data = %Data{row: row, pause_ms: pause_ms, store_max_id: store_max_id} = data) do
-    IO.inspect(data, label: "ReadFromEventStore - data")
     scope_uuid = Essig.Context.current_scope()
     events = Common.fetch_events(scope_uuid, row.max_id, Essig.Config.events_per_batch())
     multi_tuple = {Ecto.Multi.new(), data}
@@ -17,12 +16,12 @@ defmodule Projections.Runner.ReadFromEventStore do
     if length(events) > 0 do
       last_event = List.last(events)
 
-      info(data, "CURRENT MAX ID #{last_event.id}")
+      debug(data, "CURRENT MAX ID #{last_event.id}")
       # not sure, what to do with response. BUT: projections MUST NEVER fail.
       {:ok, _multi_results} = Essig.Repo.transaction(multi) |> IO.inspect()
 
       if last_event.id != store_max_id do
-        info(data, "paused for #{pause_ms}ms...")
+        debug(data, "paused for #{pause_ms}ms...")
 
         row =
           Common.update_external_state(data, row, %{max_id: last_event.id, seq: last_event.seq})
@@ -36,7 +35,7 @@ defmodule Projections.Runner.ReadFromEventStore do
         {:keep_state, %Data{data | row: row}, actions}
       else
         # finished...
-        info(data, "finished")
+        debug(data, "finished")
 
         row =
           Common.update_external_state(data, row, %{
@@ -48,13 +47,13 @@ defmodule Projections.Runner.ReadFromEventStore do
         {:next_state, :idle, %Data{data | row: row}}
       end
     else
-      info(data, "EMPTY EVENTS")
+      debug(data, "EMPTY EVENTS")
       row = Common.update_external_state(data, row, %{status: :idle})
       {:next_state, :idle, %Data{data | row: row}}
     end
   end
 
-  def info(data, msg) do
-    Logger.info("Projection #{inspect(data.name)}: #{msg}")
+  def debug(data, msg) do
+    Logger.debug("Projection #{inspect(data.name)}: #{msg}")
   end
 end
